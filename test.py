1b45856eff717c48b8c1b96e8c0ec701eab4ae99
root <root@ip-172-31-24-149.us-west-1.compute.internal>
2016-07-02 10:09:45 +0000

    add a python shell to change the mysql

---
+++ test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# coding: utf-8
+import sys 
+import MySQLdb
+
+def change_the_option_value_by_ec2_public_ip(public_ip, mysql_host):
+    sql_message = "UPDATE wp_options SET option_value='"+public_ip+"' WHERE option_name='siteurl' OR option_name='home'"
+    db = MySQLdb.connect(mysql_host,"root","root123456","wordpress")
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql_message) 
+        db.commit()
+    except Exception as e:
+        print "change the option value by ec2 public ip:\n",e
+        db.rollback()    
+    db.close()
+
+if __name__ == "__main__":
+    public_ip = sys.argv[1]
+    try: mysql_host = sys.argv[2] 
+    except: mysql_host = "testwordpress.ce7ceraac6av.us-west-1.rds.amazonaws.com"
+    change_the_option_value_by_ec2_public_ip(public_ip, mysql_host)
